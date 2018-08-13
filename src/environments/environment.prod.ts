b01ae8d45d75af79faefee01a676e0f074f6834a
Chittrang Mishra <chittrang.m@HCL.COM>
2018-08-13 16:51:46 +0530

    mapbox

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  accessTokenMapbox: 'pk.eyJ1IjoiY2hpdHRyYW5nIiwiYSI6ImNqa25wdTFkdDI0M2czcXAwZXMwd3BtcGMifQ.N16-ZHDLbfuI4M2gkqoZoQ',//'pk.eyJ1Ijoid3lra3NzIiwiYSI6ImNqMjR6aTdmdzAwNHMzMnBvbjBucjlqNm8ifQ.6GjGpofWBVaIuSnhdXQb5w',
+  accessTokenMapbox: 'pk.eyJ1IjoiY2hpdHRyYW5nIiwiYSI6ImNqa3M2bjIzNDNwcmEzcXBqZml5djJ4ajUifQ.IrkYsZRoXH5o5WTS83zNHw',//'pk.eyJ1Ijoid3lra3NzIiwiYSI6ImNqMjR6aTdmdzAwNHMzMnBvbjBucjlqNm8ifQ.6GjGpofWBVaIuSnhdXQb5w',
   directionsURL : 'https://api.mapbox.com/directions/v5/mapbox/driving-traffic/'
 };
 
